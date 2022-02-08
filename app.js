05acce1a88be8060e84b6bea720dc129c89ba4c7
Jeff Ripke <jripke74@gmail.com>
2022-02-08 11:27:22 -0800

    add logic to store recommend restaurant to file via JSON

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
@@ -5,6 +6,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'index.html');
@@ -19,6 +21,20 @@ app.get('/restaurants', function(req, res) {
 app.get('/recommend', function(req, res) {
   const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
   res.sendFile(htmlFilePath);
+});
+
+app.post('/recommend', function(req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect('/confirm');
 });
 
 app.get('/confirm', function(req, res) {
