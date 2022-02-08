df97dec22e3a30284af46e963e37a6671c394adf
Jeff Ripke <jripke74@gmail.com>
2022-02-08 15:54:30 -0800

    add loop for restaurant array

--- app.js
+++ app.js
@@ -11,24 +11,27 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/restaurants', function(req, res) {
+app.get('/restaurants', function (req, res) {
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
 
-  res.render('restaurants', { numberOfRestaurants: storedRestaurants.length });
+  res.render('restaurants', {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 
-app.get('/recommend', function(req, res) {
+app.get('/recommend', function (req, res) {
   res.render('recommend');
 });
 
-app.post('/recommend', function(req, res) {
+app.post('/recommend', function (req, res) {
   const restaurant = req.body;
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
@@ -42,11 +45,11 @@ app.post('/recommend', function(req, res) {
   res.redirect('/confirm');
 });
 
-app.get('/confirm', function(req, res) {
+app.get('/confirm', function (req, res) {
   res.render('confirm');
 });
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
   res.render('about');
 });
 
