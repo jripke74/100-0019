c515426ec5c1db3f54f86d2267b13db570c93502
Jeff Ripke <jripke74@gmail.com>
2022-02-11 10:50:26 -0800

    add query params to change restaurant order

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,22 +6,43 @@ const resData = require('../util/restaurant-data');
 const router = express.Router();
 
 router.get('/restaurants', function (req, res) {
+  let order = req.query.order;
+  let nextOrder = 'desc';
+
+  if (order != 'asc' && order != 'desc') {
+    order = 'asc';
+  }
+
+  if (order === 'desc') {
+    nextOrder = 'asc';
+  }
   const storedRestaurants = resData.getStoredRestaurants();
+
+  storedRestaurants.sort(function (resA, resB) {
+    if (
+      (order === 'asc' && resA.name > resB.name) ||
+      (order === 'desc' && resB.name > resA.name)
+    ) {
+      return 1;
+    }
+    return -1;
+  });
 
   res.render('restaurants', {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
+    nextOrder: nextOrder,
   });
 });
 
-router.get('/restaurants/:id', function(req, res) {
+router.get('/restaurants/:id', function (req, res) {
   const restaurantId = req.params.id;
   const storedRestaurants = resData.getStoredRestaurants();
 
   for (const restaurant of storedRestaurants) {
     if (restaurant.id === restaurantId) {
       return res.render('restaurant-detail', { restaurant: restaurant });
-    } 
+    }
   }
 
   res.render('404');
