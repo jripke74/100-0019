5f5ffd259b61d376f8e3b789ce0c05d97de95ea7
Jeff Ripke <jripke74@gmail.com>
2022-02-11 14:00:44 -0800

    add spread operator

--- functions.js
+++ functions.js
@@ -4,3 +4,17 @@ function greetUser(greetingPrefix, userName = 'user') {
 
 greetUser('Hello', 'Jeff');
 greetUser('Hello');
+
+function sumUp(...numbers) {
+  let result = 0;
+
+  for (const number of numbers) {
+    result += number;
+  }
+
+  return result;
+}
+
+const inputNumbers = [1, 2, 3, 5, 10, 11];
+
+console.log(sumUp(...inputNumbers));
