8f72617c0ced795a87943ad082222ceab9ed5c0d
Jeff Ripke <jripke74@gmail.com>
2022-02-14 12:26:23 -0800

    add fs.promises

--- async.js
+++ async.js
@@ -1,12 +1,22 @@
-const fs = require('fs');
+const { fuchsia } = require('color-name');
+const fs = require('fs/promises');
 
 function readFile() {
   let fileData;
-  
-  fileData = fs.readFile('data.txt', function(error, fileData) {
-    console.log('File parsing done!');
-    console.log(fileData.toString());
-  });
+
+  // fileData = fs.readFile('data.txt', function(error, fileData) {
+  //   console.log('File parsing done!');
+  //   console.log(fileData.toString());
+
+  // });
+
+  fs.readFile('data.txt')
+    .then(function (fileData) {
+      console.log('File parsing done!');
+      console.log(fileData.toString());
+      // return anotherAsyncOperation;
+    })
+    .then(function () {});
 
   console.log('Hi there!');
 }
